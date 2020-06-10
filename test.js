85f45a1510df647c546cf943d18bb450d9f318a0
NhatMinh123-Man <66712345+NhatMinh123-Man@users.noreply.github.com>
2020-06-10 14:20:54 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,28 +1,14 @@
-const { spawn } = require('child_process');
-const request = require('request');
-const test = require('tape');
-
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
-
-test('responds to requests', (t) => {
-  t.plan(4);
-
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
-
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started on Heroku with Node.js"), -1);
-    });
-  });
-});
+var http = require('http');
+var lanthu = 0;
+function XuLyWED(req, res) {
+    lanthu++;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Sever V3 - Chao moi nguoi!' +
+        "<BR> http://127.0.1.1:8000/" +
+        "<BR> localhost:8080" +
+        "<BR> lan thu -" + lanthu
+    );
+    console.log("\n\t...lan thu - " + lanthu);
+}
+http.createServer(XuLyWED).listen(8080);
+console.log("Testjs dang Run o Sever !");
